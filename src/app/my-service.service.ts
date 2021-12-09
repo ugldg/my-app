23e83a72b6334b0e3410f7ca525301aea01fe879
ugldg <hugo.ledig.auditeur@lecnam.net>
2021-12-09 11:01:41 +0100

    add myservice

---
+++ src/app/my-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+
+  constructor() { }
+
+  postClient() : number{
+    return 1;
+  }
+}
